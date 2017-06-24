698eef254fe7edaf5068e1099de6eb78706f5fe3
Kushn_000 <sergey.kushnir@outlook.com>
2017-06-24 09:18:48 +0300

    lesson14 init

---
+++ src/lesson14/Demo.java
@@ -0,0 +1,23 @@
+package lesson14;
+
+/**
+ * Created by Kushn_000 on 24.06.2017.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Object objectTest = new Object();
+
+        System.out.println(objectTest.toString());
+        System.out.println(objectTest.getClass().getName());
+
+        objectTest = new Object();
+
+        System.out.println(objectTest.toString());
+        System.out.println(objectTest.getClass().getName());
+
+        objectTest = new Object();
+
+        System.out.println(objectTest.toString());
+        System.out.println(objectTest.getClass().getName());
+    }
+}

2cb77bad5157656d051a6baf1abbc2a97fef45a3
Kushn_000 <sergey.kushnir@outlook.com>
2017-06-24 21:47:49 +0300

    lesson15.equals init

---
+++ src/lesson15/equals/User.java
@@ -0,0 +1,12 @@
+package lesson15.equals;
+
+/**
+ * Created by Kushn_000 on 24.06.2017.
+ */
+public class User {
+    private long id;
+
+    public User(long id) {
+        this.id = id;
+    }
+}

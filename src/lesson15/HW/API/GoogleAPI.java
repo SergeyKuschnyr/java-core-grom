dc9e9cdcaa1f8c468af2a3edbd9734c7d6608a02
Kushn_000 <sergey.kushnir@outlook.com>
2017-07-04 20:20:43 +0300

    googleAPI correct

--- src/lesson15/HW/API/GoogleAPI.java
+++ src/lesson15/HW/API/GoogleAPI.java
@@ -13,20 +13,67 @@ public class GoogleAPI implements API {
 
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
+        int index = 0;
+        for (Room room : rooms) {
+            if (room != null) {
+                if (price == room.getPrice() && persons == room.getPersons() && city == room.getCityName() &&
+                        hotel == room.getHotelName()) {
+                    index++;
+                }
+            }
+        }
+        if (index != 0) {
+            Room[] resultRoom = new Room[index];
+            for (int i = 0; i < rooms.length; i++) {
+                if (rooms[i] != null) {
+                    if (price == rooms[i].getPrice() && persons == rooms[i].getPersons() &&
+                            city == rooms[i].getCityName() && hotel == rooms[i].getHotelName()) {
+                        resultRoom[i] = rooms[i];
+                    }
+                }
+            }
+            return resultRoom;
+        }
+        return null;
+    }
+
+    @Override
+    public Room[] getAll() {
+        int index = 0;
+        for (Room room : rooms) {
+            if (room != null) {
+                index++;
+            }
+        }
+        if (index != 0) {
+            Room[] resultRoom = new Room[index];
+            for (int i = 0; i < rooms.length; i++) {
+                if (rooms[i] != null)
+                    resultRoom[i] = rooms[i];
+            }
+            return resultRoom;
+        }
+        return null;
+    }
+
+/*    @Override
+    public Room[] findRooms(int price, int persons, String city, String hotel) {
         Room[] tempRoom = new Room[rooms.length];
         int index = 0;
         for (Room room : rooms) {
-            if (room != null)
+            if (room != null) {
                 if (price == room.getPrice() && persons == room.getPersons() && city == room.getCityName() &&
                         hotel == room.getHotelName()) {
                     tempRoom[index] = room;
                     index++;
                 }
+            }
         }
         if (index != 0) {
             Room[] resultRoom = new Room[index];
-            for (int i = 0; i < resultRoom.length; i++)
+            for (int i = 0; i < resultRoom.length; i++) {
                 resultRoom[i] = tempRoom[i];
+            }
             return resultRoom;
         }
         return null;
@@ -49,5 +96,7 @@ public class GoogleAPI implements API {
             return resultRoom;
         }
         return null;
-    }
+    }*/
+
 }
+

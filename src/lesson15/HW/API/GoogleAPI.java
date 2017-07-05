6d073969b2e0a9672dfe87692f0349a7aea38a8f
Kushn_000 <sergey.kushnir@outlook.com>
2017-07-05 20:32:16 +0300

    findRooms correct(2)

--- src/lesson15/HW/API/GoogleAPI.java
+++ src/lesson15/HW/API/GoogleAPI.java
@@ -11,73 +11,73 @@ public class GoogleAPI implements API {
         this.rooms = rooms;
     }
 
-/*    @Override
+    @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
         int index = 0;
         for (Room room : rooms) {
             if (room != null) {
-                if (price == room.getPrice() && persons == room.getPersons() && city == room.getCityName() &&
-                        hotel == room.getHotelName()) {
+                if (room.equals(new Room(price, persons, city, hotel))) {
                     index++;
                 }
             }
         }
         if (index != 0) {
             Room[] resultRoom = new Room[index];
-            for (int i = 0; i < rooms.length; i++) {
-                if (rooms[i] != null) {
-                    if (price == rooms[i].getPrice() && persons == rooms[i].getPersons() &&
-                            city == rooms[i].getCityName() && hotel == rooms[i].getHotelName()) {
-                        resultRoom[i] = rooms[i];
+            int i = 0;
+            for (Room room : rooms) {
+                if (rooms != null) {
+                    if (rooms.equals(new Room(price, persons, city, hotel))) {
+                        resultRoom[i] = room;
+                        i++;
                     }
                 }
             }
             return resultRoom;
         }
         return null;
-    }*/
-
-   /* @Override
-    public Room[] getAll() {
-        int index = 0;
-        for (Room room : rooms) {
-            if (room != null) {
-                index++;
-            }
-        }
-        if (index != 0) {
-            Room[] resultRoom = new Room[index];
-            for (int i = 0; i < rooms.length; i++) {
-                if (rooms[i] != null)
-                    resultRoom[i] = rooms[i];
-            }
-            return resultRoom;
-        }
-        return null;
-    }*/
-
-    @Override
-    public Room[] findRooms(int price, int persons, String city, String hotel) {
-        Room[] tempRoom = new Room[rooms.length];
-        int index = 0;
-        for (Room room : rooms) {
-            if (room != null) {
-                if (price == room.getPrice() && persons == room.getPersons() && city == room.getCityName() &&
-                        hotel == room.getHotelName()) {
-                    tempRoom[index] = room;
-                    index++;
-                }
-            }
-        }
-        if (index != 0) {
-            Room[] resultRoom = new Room[index];
-            for (int i = 0; i < resultRoom.length; i++) {
-                resultRoom[i] = tempRoom[i];
-            }
-            return resultRoom;
-        }
-        return null;
     }
+//
+//    @Override
+//    public Room[] getAll() {
+//        int index = 0;
+//        for (Room room : rooms) {
+//            if (room != null) {
+//                index++;
+//            }
+//        }
+//        if (index != 0) {
+//            Room[] resultRoom = new Room[index];
+//            for (int i = 0; i < rooms.length; i++) {
+//                if (rooms[i] != null)
+//                    resultRoom[i] = rooms[i];
+//            }
+//            return resultRoom;
+//        }
+//        return null;
+//    }
+
+//    @Override
+//    public Room[] findRooms(int price, int persons, String city, String hotel) {
+//        Room[] tempRoom = new Room[rooms.length];
+//        int index = 0;
+//        for (Room room : rooms) {
+//            if (room != null) {
+//                if (price == room.getPrice() && persons == room.getPersons() && city == room.getCityName() &&
+//                        hotel == room.getHotelName()) {
+//                    tempRoom[index] = room;
+//                    index++;
+//                }
+//            }
+//        }
+//        if (index != 0) {
+//            Room[] resultRoom = new Room[index];
+//            for (int i = 0; i < resultRoom.length; i++) {
+//                resultRoom[i] = tempRoom[i];
+//            }
+//            return resultRoom;
+//        }
+//        return null;
+//    }
 
     @Override
     public Room[] getAll() {
@@ -93,7 +93,7 @@ public class GoogleAPI implements API {
             Room[] resultRoom = new Room[index];
             for (int i = 0; i < resultRoom.length; i++)
                 resultRoom[i] = tempRoom[i];
-            return resultRoom;
+             return resultRoom;
         }
         return null;
     }

48e5e8ab7bcbaf6b5ea35aae17d8adae261936fc
Kushn_000 <sergey.kushnir@outlook.com>
2017-07-06 21:49:23 +0300

    corrected(2)

--- src/lesson16/StringCompare.java
+++ src/lesson16/StringCompare.java
@@ -11,7 +11,7 @@ public class StringCompare {
         System.out.println(new String("abc") == new String("abc"));
         System.out.println(new String("abc").equals(new String("abc")));
         System.out.println("abc" == "abc");
-        System.out.println("abc".equals("abc"));
+        System.out.println("Abc".equals("abc"));
 
         String s1 = "test";
         String s2 = "test";

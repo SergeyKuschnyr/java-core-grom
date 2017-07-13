8583e3600282957325ab2ef2a4bf4a6eb0e48eb9
Kushn_000 <sergey.kushnir@outlook.com>
2017-07-13 22:47:39 +0300

    validate renew

--- src/lesson17/Solution.java
+++ src/lesson17/Solution.java
@@ -12,7 +12,7 @@ public class Solution {
         String string2 = "\"Открыв дверь, Путин протягивает руку. Возможно, он хотел либо помочь пассажиру выйти, либо взять пиджак с заднего сидения машины, поскольку он одет в рубашку, - написал \"Дождь\" в расширенном варианте заметки. - Лица пассажира не видно, но на более качественных кадрах, которые \"Дождю\" предоставило агентство Ruptly, видно, что на коленях у человека красная сумка. Сам этот человек одет неофициально - видно, что он в одежде с коротким рукавом. Неясно, мужчина это или женщина\".";
         // String string3 = null;
         String wrongString = "!@#$% +_)(* *&^%$";
-        String internetAddress = "https://www.gromcode.com";
+        String internetAddress = "http://gromcode.com";
 
         System.out.println(countWords(string));
         System.out.println(maxWord(string));
@@ -104,49 +104,38 @@ public class Solution {
     }
 
     public static boolean validate(String address) {
-        String[] strings = address.split("//");
-        if (strings.length < 2)
-            return false;
-
-        if (!(strings[0].equals("http:") || strings[0].equals("https:")))
-            return false;
-
-        String[] strings1 = strings[1].split("\\.");
-        if (!(1 < strings1.length && strings1.length < 4))
-            return false;
-
-        int i = 0;
-        if (strings1.length == 3) {
-            if (!strings1[0].equals("www"))
+        if (address != null && address.length() > 11) {
+            String[] strings = address.split("//");
+            if (strings.length != 2)
                 return false;
-            i = 1;
+
+            if (!(strings[0].equals("http:") || strings[0].equals("https:")))
+                return false;
+
+            String[] strings1 = strings[1].split("\\.");
+            if (!(1 < strings1.length && strings1.length < 4))
+                return false;
+
+            int i = 0;
+            if (strings1.length == 3) {
+                if (!strings1[0].equals("www"))
+                    return false;
+                i = 1;
+            }
+
+            char[] ch = strings1[i].toCharArray();
+            for (char c : ch)
+                if (!Character.isLetter(c))
+                    return false;
+
+            String[] zoneArr = {"com", "org", "net"};
+            for (String str : zoneArr)
+                if (str.equals(strings1[i + 1]))
+                    return true;
+            return false;
         }
-
-        char[] ch = strings1[i].toCharArray();
-        for (char c : ch)
-            if (!Character.isLetter(c))
-                return false;
-
-
-        String[] zoneArr = {"com", "org", "net"};
-        for (String str : zoneArr)
-            if (str.equals(strings1[i + 1]))
-                return true;
         return false;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 

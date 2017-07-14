1ebceaf80103e3af23bec112e5b3d711a0216897
Kushn_000 <sergey.kushnir@outlook.com>
2017-07-14 15:59:14 +0300

    maxWord renew(8)

--- src/lesson17/Solution.java
+++ src/lesson17/Solution.java
@@ -56,23 +56,26 @@ public class Solution {
 
 
     public static String maxWord(String input) {
-        if (input == null || input.length() < 5)
+        if (input == null || input.length() < 5) {
             return null;
+        }
         String[] strings = input.split(" ");
-        if (!(strings.length > 1))
+        if (!(strings.length > 1)) {
             return null;
+        }
         String maxString = "";
         //если во входящих данных нет вообще слов подходящих, что будет?
         for (String string : strings) {
             //ты { скобки потерял после ифов , и у тебя maxString = string; всегда выполняеьтся
-            if (string.length() > 0 && checkLetter(string)){
-                if (string.length() > maxString.length()){
+            if (string.length() > 0 && checkLetter(string)) {
+                if (string.length() > maxString.length()) {
                     maxString = string;
                 }
             }
         }
-        if (maxString == "")
+        if (maxString == "") {
             return null;
+        }
         return maxString;
     }
 

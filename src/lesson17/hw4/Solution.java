da4adc63d312b8080d20ec16ba32bbe2ce50fe3f
Kushn_000 <sergey.kushnir@outlook.com>
2017-07-16 18:25:27 +0300

    attempt # two

--- src/lesson17/hw4/Solution.java
+++ src/lesson17/hw4/Solution.java
@@ -17,6 +17,7 @@ public class Solution {
                 "или женщина\".";
 
 //    String wrongString = "!@#$% +_)(* *&^%$";
+        //String internetAddress = "http://g.com";
         String internetAddress = "https://www.gromcode.com";
 
         System.out.println(validate(internetAddress));
@@ -25,32 +26,39 @@ public class Solution {
     public static boolean validate(String address) {
         if (address != null && address.length() > 11) {
             String[] strings = address.split("//");
-            if (strings.length != 2)
+            if (strings.length != 2) {
                 return false;
+            }
 
-            if (!(strings[0].equals("http:") || strings[0].equals("https:")))
+            if (!(strings[0].equals("http:") || strings[0].equals("https:"))) {
                 return false;
+            }
 
             String[] strings1 = strings[1].split("\\.");
-            if (!(1 < strings1.length && strings1.length < 4))
+            if (!(1 < strings1.length && strings1.length < 4)) {
                 return false;
+            }
 
             int i = 0;
             if (strings1.length == 3) {
-                if (!strings1[0].equals("www"))
+                if (!strings1[0].equals("www")) {
                     return false;
+                }
                 i = 1;
             }
 
             char[] ch = strings1[i].toCharArray();
-            for (char c : ch)
-                if (!Character.isLetter(c))
+            for (char c : ch) {
+                if (!Character.isLetter(c)) {
                     return false;
-
+                }
+            }
             String[] zoneArr = {"com", "org", "net"};
-            for (String str : zoneArr)
-                if (str.equals(strings1[i + 1]))
+            for (String str : zoneArr) {
+                if (str.equals(strings1[i + 1])) {
                     return true;
+                }
+            }
             return false;
         }
         return false;

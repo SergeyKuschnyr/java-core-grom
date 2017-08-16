9c4db8204b18258351af14f77f1304e9bd4a562e
Kushn_000 <sergey.kushnir@outlook.com>
2017-08-16 22:04:35 +0300

    task2 corrected 2

--- src/lesson20/task2/Utils.java
+++ src/lesson20/task2/Utils.java
@@ -4,13 +4,13 @@ package lesson20.task2;
  * Created by Kushn_000 on 15.08.2017.
  */
 public class Utils {
-    private int limitTransactionPerDayCount = 10;
+    private int limitTransactionsPerDayCount = 10;
     private int limitTransactionPerDayAmount = 100;
     private int limitSimpleTransactionAmount = 40;
     private String[] cities = {"Kiev", "Odessa"};
 
-    public int getLimitTransactionPerDayCount() {
-        return limitTransactionPerDayCount;
+    public int getLimitTransactionsPerDayCount() {
+        return limitTransactionsPerDayCount;
     }
 
     public int getLimitTransactionPerDayAmount() {

fc13f744d85abb5d4fa96f2a526e5e9ccee461b3
Kushn_000 <sergey.kushnir@outlook.com>
2017-11-04 16:45:14 +0200

    corrected 15

--- src/lesson31/HW/Solution.java
+++ src/lesson31/HW/Solution.java
@@ -10,67 +10,60 @@ public class Solution {
     public Map<Character, Integer> countSymbols(String text) {
 
         char[] chars = text.toCharArray();
+        ArrayList<Character> characterArrayList = new ArrayList<>();
+        for (char ch : chars) {
+            characterArrayList.add(ch);
+        }
+
         Map<Character, Integer> tempChar = new Hashtable<>();
 
-        int count = 0;
-
-        for (int i = 0; i < chars.length; i++) {
-            if (Character.isLetter(chars[i])) {
-                for (int j = i + 1; j < chars.length; j++) {
-                    if (chars[i] == chars[j]) {
-                        count++;
-                        chars[j] = ' ';
-                    }
-                }
-                tempChar.put(chars[i], count + 1);
-            }
-            chars[i] = ' ';
-            count = 0;
+        for (Character ch : characterArrayList) {
+            if (Character.isLetter(ch))
+                tempChar.put(ch, tempChar.get(ch) == null ? 1 : tempChar.get(ch) + 1);
         }
         return tempChar;
     }
 
     public Map words(String text) {
-        String[] strArray = text.split(" ");
-        Map<String, Integer> strInfo = new Hashtable<>();
 
-        deleteWrongWord(strArray);
-
-        int count = 0;
-
-        for (int i = 0; i < strArray.length; i++) {
-            if (strArray[i] != null) {
-                for (int j = i + 1; j < strArray.length; j++) {
-                    if (strArray[i].equals(strArray[j])) {
-                        count++;
-                        strArray[j] = null;
-                    }
-                }
-                strInfo.put(strArray[i], count + 1);
-            }
-            strArray[i] = null;
-            count = 0;
+        String[] strings = text.split(" ");
+        ArrayList<String> stringAL = new ArrayList<>();
+        for (String str : strings) {
+            stringAL.add(str);
         }
-        return strInfo;
-    }
 
-    private void deleteWrongWord(String[] strArray) {
-        for (int i = 0; i < strArray.length; i++) {
-            if (strArray[i].length() <= 2) {
-                strArray[i] = null;
+        Map<String, Integer> htArray = new Hashtable<>();
+
+        for (int i = 0; i < stringAL.size(); i++) {
+            if (stringAL.get(i).length() <= 2) {
+                stringAL.remove(i);
                 continue;
             }
-            if (!Character.isLetter(strArray[i].charAt(strArray[i].length() - 1))) {
-                strArray[i] = strArray[i].substring(0, (strArray[i].length() - 1));
-            }
-            for (int j = 0; j < strArray[i].length(); j++) {
-                if (!Character.isLetter(strArray[i].charAt(j))) {
-                    if (!(strArray[i].charAt(j) == '-')) {
-                        strArray[i] = null;
-                        break;
-                    }
+            if (!Character.isLetter(stringAL.get(i).charAt(stringAL.get(i).length() - 1))) {
+                String str = stringAL.get(i);
+                stringAL.remove(i);
+                if (stringAL.get(i).length() - 1 >= i) {
+                    stringAL.set(i, str.substring(0, (str.length() - 2)));
                 }
+                stringAL.add(str.substring(0, (str.length() - 2)));
+            }
+            if (digitInWord(stringAL.get(i))) {
+                stringAL.remove(i);
+                continue;
+            }
+            htArray.put(stringAL.get(i), htArray.get(stringAL.get(i)) == null ? 1 : htArray.get(stringAL.get(i)) + 1);
+        }
+        return htArray;
+    }
+
+    private boolean digitInWord(String string) {
+        char[] chars = string.toCharArray();
+        for (char ch : chars) {
+            if (!Character.isLetter(ch) && ch != '-') {
+                return true;
             }
         }
+        return false;
     }
 }
+

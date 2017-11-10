31a8aea257840dc986ca8656a1728c661d8d9836
Kushn_000 <sergey.kushnir@outlook.com>
2017-11-10 21:21:13 +0200

    corrected 25

--- src/lesson32/readingNumber/Solution.java
+++ src/lesson32/readingNumber/Solution.java
@@ -4,15 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  * Created by Kushn_000 on 06.11.2017.
  */
 public class Solution {
     public void readNumbers() throws IOException {
-        ArrayList<Integer> intAL = new ArrayList();
-        int sum = 0;
+        ArrayList<Integer> intAL = new ArrayList<>();
 
         InputStreamReader reader = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(reader);
@@ -20,24 +18,14 @@ public class Solution {
 
         for (int i = 0; i < 3; i++) {
             System.out.println("Input 10 numbers: ");
-
             String[] strings = br.readLine().split(" ");
-            if (strings.length != 10) {
-                System.out.println("Your numbers are wrong. You have " + (2 - i) + " attempts to try again");
-                counter++;
-                continue;
-            }
-            for (String str : strings) {
-                intAL.add(Integer.parseInt(str));
-            }
 
-            if (!numberValidate(intAL)) {
+            createArrayList(strings, intAL);
+
+            if (!validator(intAL)) {
                 System.out.println("Your numbers are wrong. You have " + (2 - i) + " attempts to try again");
             } else {
-                for (Integer integer : intAL) {
-                    sum += integer;
-                }
-                System.out.println("Sum: " + sum);
+                sumCounting(intAL);
                 break;
             }
             intAL.clear();
@@ -47,12 +35,29 @@ public class Solution {
             System.out.println("Your numbers are wrong. Number of attempts exceeded");
     }
 
-    private boolean numberValidate(ArrayList<Integer> intAL) {
+    private boolean validator(ArrayList<Integer> intAL) {
+        if (intAL.size() != 10)
+            return false;
         for (Integer integer : intAL) {
             if (integer > 100) {
                 return false;
             }
         }
         return true;
+    }
+
+    private void sumCounting(ArrayList<Integer> intAL) {
+        int sum = 0;
+        for (Integer integer : intAL) {
+            sum += integer;
+        }
+        System.out.println("Sum: " + sum);
+    }
+
+    private void createArrayList(String[] strings, ArrayList intAL){
+        for (String str : strings) {
+            if (!str.isEmpty())
+                intAL.add( Integer.parseInt(str));
+        }
     }
 }

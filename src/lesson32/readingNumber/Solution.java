6e803762e10d662fdc7f85918d34a0cdfa8de420
Kushn_000 <sergey.kushnir@outlook.com>
2017-11-22 08:21:18 +0200

    corrected 47

--- src/lesson32/readingNumber/Solution.java
+++ src/lesson32/readingNumber/Solution.java
@@ -1,69 +1,59 @@
 package lesson32.readingNumber;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.*;
+import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * Created by Kushn_000 on 06.11.2017.
  */
 public class Solution {
     public void readNumbers() throws IOException {
-        ArrayList<Integer> intAL = new ArrayList<>();
+        int attemptCount = 3;
 
-        InputStreamReader reader = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(reader);
-        int counter = 0;
+        while (attemptCount != 0) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Input 10 numbers: ");
-            String[] strings = br.readLine().split(" ");
+            String string = br.readLine();
+            String[] items = string.split(" ");
 
-            for (String str : strings) {
-                if (!str.isEmpty() && !hasNumberAtWord(str)) {
-                    intAL.add(Integer.parseInt(str));
-                }
-            }
-
-            if (!validator(intAL)) {
-                if (i == 2){
+            if (!validator(items)) {
+                if (attemptCount == 1) {
                     System.out.println("Your numbers are wrong. Number of attempts exceeded");
-                    break;
+                    return;
                 }
-                System.out.println("Your numbers are wrong. You have " + (2 - i) + " attempts to try again");
-            } else {
-                int sum = 0;
-                for (Integer integer : intAL) {
-                    sum += integer;
-                }
-                System.out.println("Sum: " + sum);
-                break;
+                System.out.println("Your numbers are wrong. You have " +
+                        --attemptCount + " attempts to try again");
+                continue;
             }
-            intAL.clear();
-            counter++;
+
+            int res = 0;
+            for (String str : items){
+                res += Integer.parseInt(str);
+            }
+            System.out.println(res);
+            return;
         }
-        if (counter == 3)
-            System.out.println("Your numbers are wrong. Number of attempts exceeded");
     }
 
-    private boolean validator(ArrayList<Integer> intAL) {
-        if (intAL.size() != 10)
+    private boolean validator(String[] strings) {
+        if (strings.length != 10) {
             return false;
-        for (Integer integer : intAL) {
-            if (integer > 100) {
-                return false;
+        }
+
+        for (String string : strings) {
+            for (char ch : string.toCharArray()) {
+                if (Character.isLetter(ch))
+                    return false;
             }
+            if (Integer.parseInt(string) > 100)
+                return false;
         }
         return true;
     }
 
-    private boolean hasNumberAtWord(String str) {
-        for (int i = 0; i < str.length(); i++) {
-            if (!Character.isLetter(str.charAt(i))) {
-                return false;
-            }
-        }
-        return true;
-    }
 }

fd45dab52c5c8ab787bea568eecf1b5eb2c5f3d3
Kushn_000 <sergey.kushnir@outlook.com>
2017-11-26 14:05:20 +0200

    corrected 25

--- src/lesson34/transferFileContent/Demo.java
+++ src/lesson34/transferFileContent/Demo.java
@@ -1,7 +1,17 @@
 package lesson34.transferFileContent;
 
+
 /**
  * Created by Kushn_000 on 25.11.2017.
  */
 public class Demo {
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        try {
+            solution.transferFileContent("C:/Users/Kushn_000/Desktop/service_routing.txt",
+                    "C:/Users/Kushn_000/Desktop/test.txt");
+        }catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
---
+++ src/lesson34/transferFileContent/Solution.java
@@ -0,0 +1,70 @@
+package lesson34.transferFileContent;
+
+import java.io.*;
+
+/**
+ * Created by Kushn_000 on 25.11.2017.
+ */
+public class Solution {
+    public void transferFileContent(String fileFromPath, String fileToPath) throws Exception {
+        validate(fileFromPath, fileToPath);
+
+        writeToFile(fileToPath, readFromFile(fileFromPath));
+    }
+
+    private StringBuffer readFromFile(String fileFromPath) {
+        StringBuffer res = new StringBuffer();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileFromPath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                res.append('\n');  // ??????????????????????????????????
+                res.append(line);
+            }
+            new FileWriter(fileFromPath).close();
+        } catch (FileNotFoundException e) {
+            System.err.println("File does not exist");
+        } catch (IOException e) {
+            System.err.println("Read from file" + fileFromPath + "failed");
+        }
+        return res;
+    }
+
+    private void writeToFile(String fileToPath, StringBuffer res) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileToPath, true))) {
+            bufferedWriter.append(res);
+        } catch (Exception e) {
+            System.err.println("Can't write to file");
+        }
+    }
+
+    private void validate(String fileFromPath, String fileToPath) throws Exception {
+        File fileFrom = new File(fileFromPath);
+        File fileTo = new File(fileToPath);
+
+        if (!fileFrom.exists()) {
+            throw new FileNotFoundException("File " + fileFrom + " don't excist");
+        }
+        if (fileFrom.length() == 0) {
+            throw new Exception("File " + fileFrom + " is empty");
+        }
+        if (!fileTo.exists()) {
+            throw new FileNotFoundException("File " + fileTo + " don't excist");
+        }
+        if (!fileFrom.canRead()) {
+            throw new Exception("File " + fileFrom + " does not have permission to be read");
+        }
+        if (!fileTo.canWrite()) {
+            throw new Exception("File " + fileTo + " does not have permission to be written");
+        }
+    }
+}
+
+
+//try {
+//        PrintWriter printWriter = new PrintWriter(fileFromPath);
+//        }catch (FileNotFoundException e){
+//
+//        }
+//                System.getProperty(" ");
+//res.replace(res.length() - 1, res.length(), "");
+//            bufferedWriter.append("\n");    // ??????????????????????????
